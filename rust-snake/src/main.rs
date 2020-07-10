5aa8967984bcd3e117689286c7e7b930784fdee5
= <=>
2020-07-09 23:51:29 -0300

    now the windows has a background color!

--- rust-snake/src/main.rs
+++ rust-snake/src/main.rs
@@ -1,31 +1,30 @@
-extern crate sdl2;
 extern crate gl;
+extern crate sdl2;
 
 fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
     let window = video_subsystem
         .window("Game", 900, 700)
+        .opengl()
         .resizable()
         .build()
         .unwrap();
+
+    let _gl_context = window.gl_create_context().unwrap();
+    let _gl =
+        gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
 
     unsafe {
         gl::ClearColor(0.3, 0.3, 0.5, 1.0);
     }
 
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    let load_function = |s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void;
-    let _gl_context = window.gl_create_context().unwrap();
-    let _gl =
-        gl::load_with(load_function);
-
-
+    let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
         for event in event_pump.poll_iter() {
             match event {
-                sdl2::event::Event::Quit {..} => break 'main,
-                _ => {},
+                sdl2::event::Event::Quit { .. } => break 'main,
+                _ => {}
             }
         }
 
@@ -35,5 +34,4 @@ fn main() {
 
         window.gl_swap_window();
     }
-
 }
